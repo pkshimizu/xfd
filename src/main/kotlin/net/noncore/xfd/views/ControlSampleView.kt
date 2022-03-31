95e86d95dcc592f9c293c7772069d7a55ddb8fe9
shimizu.kenji <shimizu.kenji@noncore.net>
2022-03-31 09:45:02 +0900

    feat: コントロールの実装見本を追加

--- src/main/kotlin/net/noncore/xfd/views/ControlSampleView.kt
+++ src/main/kotlin/net/noncore/xfd/views/ControlSampleView.kt
@@ -1,13 +1,70 @@
 package net.noncore.xfd.views
 
+import javafx.collections.FXCollections
+import javafx.scene.control.ToggleGroup
+import javafx.scene.paint.Color
 import tornadofx.*
 
 class ControlSampleView: Fragment() {
+    val comboboxItems = FXCollections.observableArrayList("Tokyo", "Osaka", "Nagoya", "Sapporo", "Fukuoka")
     override val root = vbox() {
         label("Control Samples")
-        hbox() {
-            label("Button")
+        vbox {
+            label("Buttons")
             button("Button")
+        }
+        vbox {
+            label("Labels")
+            label("Blue Label") {
+                textFill = Color.BLUE
+            }
+            label("Red Label") {
+                textFill = Color.RED
+            }
+        }
+        vbox {
+            label("Text Fields")
+            textfield()
+            label("Password Field"){
+                addClass(Styles.caption)
+            }
+            passwordfield()
+        }
+        vbox {
+            label("Check Boxies")
+            checkbox("同意する")
+        }
+        vbox {
+            label("ComboBox")
+            combobox<String> {
+                items = comboboxItems
+            }
+        }
+        vbox {
+            label("Toggle Buttons")
+            togglebutton {
+                val state = selectedProperty().stringBinding {
+                    if (it == true) "ON" else "OFF"
+                }
+                textProperty().bind(state)
+            }
+            hbox {
+                togglegroup {
+                    togglebutton("One")
+                    togglebutton("Two")
+                    togglebutton("Three")
+                }
+            }
+        }
+        vbox {
+            label("Radio Buttons")
+            hbox {
+                togglegroup {
+                    radiobutton("One")
+                    radiobutton("Two")
+                    radiobutton("Three")
+                }
+            }
         }
     }
 }
--- src/main/kotlin/net/noncore/xfd/views/Styles.kt
+++ src/main/kotlin/net/noncore/xfd/views/Styles.kt
@@ -9,17 +9,39 @@ import tornadofx.px
 class Styles : Stylesheet() {
     companion object {
         val heading by cssclass()
+        val caption by cssclass()
     }
-
     init {
         label and heading {
             padding = box(10.px)
-            fontSize = 20.px
+            fontSize = 24.px
             fontWeight = FontWeight.BOLD
         }
-        button {
-            fontSize = 20.px
+        label and caption {
+            fontSize = 14.px
             fontWeight = FontWeight.BOLD
+        }
+        label {
+            fontSize = 18.px
+        }
+        button {
+            fontSize = 18.px
+            fontWeight = FontWeight.BOLD
+        }
+        textField {
+            fontSize = 18.px
+        }
+        checkBox {
+            fontSize = 18.px
+        }
+        comboBox {
+            fontSize = 18.px
+        }
+        toggleButton {
+            fontSize = 18.px
+        }
+        radioButton {
+            fontSize = 18.px
         }
     }
 }
